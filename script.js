2da1c541b52051715e356c45a7776a164c27eb4b
niwspider <140329373+niwspider@users.noreply.github.com>
2025-06-20 20:56:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// script.js - interações básicas
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('Mensagem enviada com sucesso!');
+    });
+});
+
+document.querySelectorAll(".faq-question").forEach(button => {
+  button.addEventListener("click", () => {
+    const answer = button.nextElementSibling;
+    button.classList.toggle("active");
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      document.querySelectorAll(".faq-answer").forEach(a => a.style.maxHeight = null);
+      document.querySelectorAll(".faq-question").forEach(b => b.classList.remove("active"));
+      answer.style.maxHeight = answer.scrollHeight + "px";
+      button.classList.add("active");
+    }
+  });
+});
+
+const slider = document.getElementById('slider');
+const nextBtn = document.querySelector('.next-arrow');
+const prevBtn = document.querySelector('.prev-arrow');
+const scrollAmount = 320; // valor igual ao tamanho do card + margem
+
+  nextBtn.addEventListener('click', () => {
+    slider.scrollLeft += scrollAmount;
+  });
+
+  prevBtn.addEventListener('click', () => {
+    slider.scrollLeft -= scrollAmount;
+  });
